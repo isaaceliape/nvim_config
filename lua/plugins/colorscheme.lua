b33637eb41c4bb067cbe9d2286aedd4486c4c593
Isaac Eliape <isaaceliape@gmail.com>
2025-01-06 16:41:03 +0000

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "maxmx03/fluoromachine.nvim",
+    opts = {
+      -- theme = "retrowave",
+      theme = "fluoromachine",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      theme = "retrowave",
+      colorscheme = "fluoromachine",
+    },
+  },
+}
