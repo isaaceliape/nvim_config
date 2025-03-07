6586edd54c97c5f15fb1410e83df68a93bf10518
Isaac Eliape <isaaceliape@gmail.com>
2025-03-07 12:20:06 +0000

    update

--- init.lua
+++ init.lua
@@ -7,9 +7,5 @@ vim.g.neovide_cursor_animation_length = 0
 -- copy to system clipboard
 vim.opt.clipboard = "unnamedplus"
 
--- vim.keymap.del("n", "<leader>d")
-
--- set keymaps to navigate between buffers
--- vim.keymap.set("n", "<leader>n", ":bnext<cr>", { desc = "Go to next buffer" })
--- vim.keymap.set("n", "<leader>t", ":bprev<cr>", { desc = "Go to prev buffer" })
--- vim.keymap.set("n", "<leader>d", ":bdelete<cr>", { desc = "Delete current buffer" })
+-- scrolloff "jump by half screen when navigating using ctrl+d and ctrl+u"
+vim.o.scrolloff = 999
