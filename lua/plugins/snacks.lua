afada6903c40cba44a43782a12496cb73e47915d
Eliape, Isaac <IEliape@unum.com>
2025-10-13 11:19:01 +0100

    Enable preview pane by default for Snacks explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,10 +2,12 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      layout = { preview = "main" },
       sources = {
         explorer = {
           hidden = true,
           ignored = true,
+          layout = { preview = true },
         },
         files = {
           hidden = true,
