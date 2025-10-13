9372bab6ec78a0e7d842a9ba99c7554c591febe8
Eliape, Isaac <IEliape@unum.com>
2025-10-13 12:16:46 +0100

    Disable <esc> key to close Snacks explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,11 +3,25 @@ return {
   opts = {
     picker = {
       layout = { preview = "main" },
+      win = {
+        list = {
+          keys = {
+            ["<esc>"] = false,
+          },
+        },
+      },
       sources = {
         explorer = {
           hidden = true,
           ignored = true,
           layout = { preview = "main" },
+          win = {
+            list = {
+              keys = {
+                ["<esc>"] = false,
+              },
+            },
+          },
         },
         files = {
           hidden = true,
